ee71fbe93b05a88ed37647f3de88b359636b51a7
Mehmethan AYTEKELİ <mehmethanaytekeli@gmail.com>
2023-11-07 10:13:35 +0300

    Update 12.py

--- Pyhton İle Programlamaya Giriş/12.py
+++ Pyhton İle Programlamaya Giriş/12.py
@@ -1,9 +1,15 @@
 kullanıcı_sayısı=0
 kullanıcı_sayısı=int(input("faktoriyeli değer giriniz:"))
 faktoriyel=1
-while kullanıcı_sayısı>0:
-    faktoriyel=kullanıcı_sayısı*faktoriyel
-    kullanıcı_sayısı=kullanıcı_sayısı-1
+if (kullanıcı_sayısı==0):
+    print("1")
 
-print(faktoriyel)
+elif(kullanıcı_sayısı<0):
+    print("yanlış değer girildi")
+else:
+    while kullanıcı_sayısı>0:
+        faktoriyel=kullanıcı_sayısı*faktoriyel
+        kullanıcı_sayısı=kullanıcı_sayısı-1
+
+    print(faktoriyel)
 
